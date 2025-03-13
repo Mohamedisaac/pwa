593739c09277646a54d1ca79718e785210170155
Mohamed Isaac Abdullah <77462890+Mohamedisaac@users.noreply.github.com>
2025-03-13 23:23:03 +0300

    Update app.js added service worker

--- app.js
+++ app.js
@@ -127,3 +127,17 @@ document.addEventListener('DOMContentLoaded', function() {
         if (e.key === 'Escape') hideAbout();
     });
 });
+
+// Add Service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('Service Worker registered: ', registration);
+          // Optionally, you can add code here to handle service worker updates
+        })
+        .catch(registrationError => {
+          console.log('Service Worker registration failed: ', registrationError);
+        });
+    });
+  }
